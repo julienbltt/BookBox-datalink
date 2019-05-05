a0e6d261e413fa3aeadd3e9db4fac038d4b5a4f1
julienbltt <julien.blt@yahoo.com>
2019-05-05 23:24:54 +0200

    Adding acceuil.php

---
+++ acceuil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="design/style.css">
+		<title>DATALINK - ACCEUIL</title>
+	</head>
+
+	<body>
+		<div align="center">
+
+			<h2>DATA-LINK</h2>
+			<h2>Acceuil</h2>
+			<br /><br /><br />
+			<p><a href="newAdherent.php">Nouvel ADHERENT</a></p>
+			<p><a href="newEmprunt.php">Nouvel EMPRUNT</a></p>
+			
+		</div>
+	</body>
+</html>
